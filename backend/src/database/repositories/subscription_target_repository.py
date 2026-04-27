6936c4007c66621de2aeaefbf26ed573297f67c6
Maxim Sniazhko <maks.kreet@mail.ru>
2026-04-27 09:06:05 +0300

    Remove dead code

--- backend/src/database/repositories/subscription_target_repository.py
+++
@@ -1,6 +0,0 @@
-from src.database.repositories.base_repository import BaseRepository
-from src.models.subscriptions import SubscriptionTarget
-
-
-class SubscriptionTargetRepository(BaseRepository[SubscriptionTarget]):
-    pass
--- backend/src/database/repositories/subscription_type_repository.py
+++
@@ -1,6 +0,0 @@
-from src.database.repositories.base_repository import BaseRepository
-from src.models.subscriptions import SubscriptionType
-
-
-class SubscriptionTypeRepository(BaseRepository[SubscriptionType]):
-    pass
